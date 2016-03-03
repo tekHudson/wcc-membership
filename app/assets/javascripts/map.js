a0d5c9714cd93d29d1a1662ecd29a655513ac88e
Tek Hudson <tek.hudson@gmail.com>
2016-03-03 15:24:25 -0800

    Add map to about and refactor view

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,23 @@
+function init_map() {
+    var var_location = new google.maps.LatLng(37.30619, -121.87829);
+
+    var var_mapoptions = {
+      center: var_location,
+      zoom: 11
+    };
+
+    var var_marker = new google.maps.Marker({
+        position: var_location,
+        map: var_map,
+        title:"West Coast Cigars"});
+
+    var var_map = new google.maps.Map(document.getElementById("map-container"), var_mapoptions);
+
+    var_marker.setMap(var_map);
+
+}
+
+$(document).ready(function(e, data){
+    init_map();
+});
+
